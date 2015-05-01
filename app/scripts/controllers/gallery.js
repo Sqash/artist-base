92c677d561c411428aa5e5449e378a74c03bb9ab
Sam Whiteley <sam.whiteley@hotmail.com>
2015-05-01 00:44:38 -0400

    Gallery json files all dummied up, ready to make the gallery carousels in the view.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -8,7 +8,29 @@
  * Controller of the artistBaseApp
  */
 angular.module('artistBaseApp')
-  .controller('GalleryCtrl', function ($scope, $routeParams) {
-    $scope.title = 'Works for Sale';
-    console.log($routeParams.title);
+  .controller('GalleryCtrl', function ($scope, $http, $routeParams) {
+
+    $scope.galleryName = $routeParams.title;
+
+    $http.get('assets/json/' + $routeParams.title + '.json', {
+      cache: true,
+      responseType: 'json'
+    })
+    .success(function(data) {
+      if(data) {
+        $scope.galleryTitle = data.title;
+        $scope.galleryDescription = data.description;
+        $scope.works = data.works;
+      } else {
+        $scope.galleryTitle = 'Error ):';
+        $scope.galleryDescription = 'There seems to be a problem with your ' +
+          $routeParams.title + '.json file. Probably syntax.';
+      }
+    })
+    .error(function() {
+      $scope.galleryTitle = 'Error ):';
+      $scope.galleryDescription = 'Couldn\'t find your ' + $routeParams.title +
+        '.json gallery manifest file. It should be in assets/json/';
+    });
+
   });
