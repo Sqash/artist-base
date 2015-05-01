ab8d5025d54d445de58363649b86cf6a2bac5655
Sam Whiteley <sam.whiteley@hotmail.com>
2015-05-01 11:43:55 -0400

    Cleaned up some files with small mistakes & bugs-waiting-to-happen.

--- test/spec/controllers/gallery.js
+++ test/spec/controllers/gallery.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: SidebarCtrl', function () {
+describe('Controller: GalleryCtrl', function () {
 
   // load the controller's module
   beforeEach(module('artistBaseApp'));
 
-  var SidebarCtrl,
+  var GalleryFsCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    SidebarCtrl = $controller('SidebarCtrl', {
+    GalleryFsCtrl = $controller('GalleryCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have a dummy test until I write async tests', function () {
+    expect(1).toBe(1);
   });
 });
