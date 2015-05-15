096b10b2ea1e40ed9e7cc74f1e3499524f4b7705
Sam Whiteley <sam.whiteley@hotmail.com>
2015-05-15 15:21:39 -0400

    Started on about.js unit tests

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -4,18 +4,58 @@ describe('Controller: AboutCtrl', function () {
   // load the controller's module
   beforeEach(module('artistBaseApp'));
 
-  var AboutCtrl,
-    scope;
+  var AboutCtrl, scope, json;
+
+  beforeAll(function () {
+    jasmine.getFixtures().fixturesPath = 'base/app/assets/json/';
+    var data = readFixtures('about.json');
+    json = JSON.parse(data);
+  });
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+
+    $httpBackend.when('GET', 'assets/json/about.json').respond(json);
     scope = $rootScope.$new();
+
     AboutCtrl = $controller('AboutCtrl', {
       $scope: scope
     });
+    $httpBackend.flush();
   }));
 
-  it('should have a dummy test until I figure out async ones', function () {
-    expect(1).toBe(1);
+  it('should have a string "content", if defined', function () {
+    if(scope.content) {
+      expect(scope.content).toEqual(jasmine.any(String));
+    } else {
+      console.log('About "content" is not defined');
+      return true;
+    }
   });
+
+  it('should have a string "content", if defined', function () {
+    if(scope.content) {
+      expect(scope.content).toEqual(jasmine.any(String));
+    } else {
+      console.log('About "content" is not defined');
+      return true;
+    }
+  });
+
+  it('should have an object support, if defined', function () {
+    if(scope.support) {
+      expect(scope.content).toEqual(jasmine.any(Object));
+    } else {
+      console.log('About "support" is not defined');
+      return true;
+    }
+  });
+
+  xit('should have at least 1 valid field in support', function () {
+    if(scope.support){
+    } else {
+      return true;
+    }
+  });
+
 });
