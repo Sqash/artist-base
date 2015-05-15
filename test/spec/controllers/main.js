f3f3df100dbd4bc8ad2001a7252bb7566d7a0aed
Sam Whiteley <sam.whiteley@hotmail.com>
2015-05-15 12:24:58 -0400

    main.js unit tests added

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,18 +5,51 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('artistBaseApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, scope, json;
+
+  beforeAll(function() {
+    jasmine.getFixtures().fixturesPath = 'base/app/assets/json/';
+    var data = readFixtures('home.json');
+    json = JSON.parse(data);
+  });
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+
+    $httpBackend.when('GET', 'assets/json/home.json').respond(json);
     scope = $rootScope.$new();
+
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: scope,
     });
+    $httpBackend.flush();
   }));
 
-  it('should have a dummy test until I make async ones', function () {
-    expect(1).toBe(1);
+  it('should have a string value for artist, if defined', function() {
+    if(scope.artist) {
+      expect(scope.artist).toEqual(jasmine.any(String))
+    } else {
+      console.log('Artist undefined in home.json file');
+      return true;
+    }
   });
+
+  it('should have a string value for tagline, if defined', function() {
+    if(scope.tagline) {
+      expect(scope.tagline).toEqual(jasmine.any(String))
+    } else {
+      console.log('Tagline undefined in home.json file');
+      return true;
+    }
+  });
+
+  it('should have a string value for description, if defined', function() {
+    if(scope.description) {
+      expect(scope.description).toEqual(jasmine.any(String))
+    } else {
+      console.log('Tagline undefined in home.json file');
+      return true;
+    }
+  });
+
 });
