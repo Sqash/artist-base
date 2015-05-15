acba6328b8480ac984320db4be30413b9fc30efc
Sam Whiteley <sam.whiteley@hotmail.com>
2015-05-15 15:04:05 -0400

    Added unit tests for navbar.js

--- test/spec/controllers/navbar.js
+++ test/spec/controllers/navbar.js
@@ -5,18 +5,48 @@ describe('Controller: NavbarCtrl', function () {
   // load the controller's module
   beforeEach(module('artistBaseApp'));
 
-  var NavbarCtrl,
-    scope;
+  var NavbarCtrl, scope, json;
+
+  beforeAll(function() {
+    jasmine.getFixtures().fixturesPath = 'base/app/assets/json/';
+    var data = readFixtures('nav.json');
+    json = JSON.parse(data);
+  });
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+
+    $httpBackend.when('GET', 'assets/json/nav.json').respond(json);
     scope = $rootScope.$new();
+
     NavbarCtrl = $controller('NavbarCtrl', {
       $scope: scope
     });
+    $httpBackend.flush();
   }));
 
-  it('should have a dummy test until I write async ones', function () {
-    expect(1).toBe(1);
+  it('should have menuItems', function () {
+    expect(scope.menu).toBeDefined();
+    expect(scope.menu.length).toBeGreaterThan(0);
   });
+
+  it('should have all menuItems with both a title and link', function() {
+    for(var i=0; i<scope.menu.length; i++){
+      expect(scope.menu[i].title).toEqual(jasmine.any(String));
+      expect(scope.menu[i].link).toMatch(/^\/[a-zA-Z0-9_\-\/]*$/);
+    }
+  });
+
+  it('should have branding', function () {
+    expect(scope.branding).toEqual(jasmine.any(String));
+  });
+
+  it('should have a boolean brandingIsImage attribute', function () {
+    expect(scope.brandIsImage).toEqual(jasmine.any(Boolean));
+  });
+
+  it('should have a default value of true for nav Collapse', function () {
+    expect(scope.isCollapsed).toEqual(true);
+  });
+
 });
